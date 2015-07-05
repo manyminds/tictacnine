ea995a514df1e8c9bd6a1086ea1ced5a276c61de
n.wagensonner <n.wagensonner@manyminds.de>
2015-07-05 12:42:11 +0200

    Webserver delivers html files now.

--- cmd/ttn.go
+++ cmd/ttn.go
@@ -9,12 +9,12 @@ import (
 )
 
 func main() {
-	s := server.NewServer("./")
+	//TODO move settings to env
+	s := server.NewServer("../frontend/")
 	port := 13337
 	fmt.Printf("Server started on port %d.\n", port)
 	err := http.ListenAndServe(fmt.Sprintf("127.0.0.1:%d", port), logger.Logger(s))
 	if err != nil {
 		panic(err)
 	}
-
 }
